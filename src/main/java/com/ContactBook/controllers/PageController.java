311326f7f1034640c1244fd51ce14e39b29ee7f1
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-05-27 15:52:08 +0530

    Tailwind CSS configured and tested

---
+++ src/main/java/com/ContactBook/controllers/PageController.java
@@ -0,0 +1,14 @@
+package com.ContactBook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String getHome()
+    {
+        return "home";
+    }
+}
